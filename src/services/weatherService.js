6c829575104fc35d00149b5239e199f305d05d6c
supritam1 <mohantysupritam@gmail.com>
2025-03-12 16:54:03 +0530

    Updated [feature_name] or [bug fix]

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "f785ad8e18e55277c349b2fb2d10e789";
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const fetchWeather = async (city) => {
